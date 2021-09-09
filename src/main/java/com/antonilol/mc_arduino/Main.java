cc34bb80a09a2b1ba26e57537facf56dd5e89461
Antoni Spaanderman <56turtle56@gmail.com>
2021-09-09 16:20:10 +0200

    forgot to update version

--- src/main/java/com/antonilol/mc_arduino/Main.java
+++ src/main/java/com/antonilol/mc_arduino/Main.java
@@ -7,7 +7,7 @@ import net.minecraft.client.network.ClientPlayerEntity;
 public class Main implements ClientModInitializer {
 	
 	public static final String MOD_ID = "mc_arduino"; // is this needed? i saw other people do it in tutorials
-	public static final String VERSION = "1.0.0"; // updated by updateVersion script with sed :)
+	public static final String VERSION = "1.0.1"; // updated by updateVersion script with sed :)
 	private Time prevTime = null;
 	private XP prevXP = null;
 	private boolean cleared = true;
