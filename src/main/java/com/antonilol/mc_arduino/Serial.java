58fcd58b7eb4cf2e18db49947ac30035744fe28d
Antoni Spaanderman <56turtle56@gmail.com>
2021-10-15 09:40:26 +0200

    cleanup

--- src/main/java/com/antonilol/mc_arduino/Serial.java
+++ src/main/java/com/antonilol/mc_arduino/Serial.java
@@ -25,119 +25,32 @@
 
 package com.antonilol.mc_arduino;
 
+import java.io.IOException;
+
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
 import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
-import java.nio.charset.StandardCharsets;
-
-//import static processing.app.I18n.format;
-//import static processing.app.I18n.tr;
-
 public class Serial implements SerialPortEventListener {
-
-  //PApplet parent;
-
-  // properties can be passed in for default values
-  // otherwise defaults to 9600 N81
-
-  // these could be made static, which might be a solution
-  // for the classloading problem.. because if code ran again,
-  // the static class would have an object that could be closed
+  /**
+   * General error reporting, all corraled here just in case
+   * I think of something slightly more intelligent to do.
+   */
+  private static void errorMessage(String where, Throwable e) {
+    System.err.println("Error inside Serial." + where);
+    e.printStackTrace();
+  }
 
   private SerialPort port;
 
-  private CharsetDecoder bytesToStrings;
-  private static final int IN_BUFFER_CAPACITY = 128;
-  private static final int OUT_BUFFER_CAPACITY = 128;
-  private ByteBuffer inFromSerial = ByteBuffer.allocate(IN_BUFFER_CAPACITY);
-  private CharBuffer outToMessage = CharBuffer.allocate(OUT_BUFFER_CAPACITY);
-
-  
-  public Serial(String port) throws Exception {// throws SerialException {
-    this(port, 9600, 'N', 8, 1, true, true);
-  }
-/*
-  public Serial(int irate) throws SerialException {
-    this(PreferencesData.get("serial.port"), irate,
-      PreferencesData.getNonEmpty("serial.parity", "N").charAt(0),
-      PreferencesData.getInteger("serial.databits", 8),
-      PreferencesData.getFloat("serial.stopbits", 1),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableRTS"),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableDTR"));
-  }
-
-  public Serial(String iname, int irate) throws SerialException {
-    this(iname, irate, PreferencesData.getNonEmpty("serial.parity", "N").charAt(0),
-      PreferencesData.getInteger("serial.databits", 8),
-      PreferencesData.getFloat("serial.stopbits", 1),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableRTS"),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableDTR"));
-  }
-
-  public Serial(String iname) throws SerialException {
-    this(iname, PreferencesData.getInteger("serial.debug_rate", 9600),
-      PreferencesData.getNonEmpty("serial.parity", "N").charAt(0),
-      PreferencesData.getInteger("serial.databits", 8),
-      PreferencesData.getFloat("serial.stopbits", 1),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableRTS"),
-      !BaseNoGui.getBoardPreferences().getBoolean("serial.disableDTR"));
-  }
-
-  public static boolean touchForCDCReset(String iname) throws SerialException {
-    SerialPort serialPort = new SerialPort(iname);
-    try {
-      serialPort.openPort();
-      serialPort.setParams(1200, 8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-      serialPort.setDTR(false);
-      serialPort.closePort();
-      return true;
-    } catch (SerialPortException e) {
-      throw new SerialException(format(tr("Error touching serial port ''{0}''."), iname), e);
-    } finally {
-      if (serialPort.isOpened()) {
-        try {
-          serialPort.closePort();
-        } catch (SerialPortException e) {
-          // noop
-        }
-      }
-    }
-  }
-*/
-  protected Serial(String iname, int irate, char iparity, int idatabits, float istopbits, boolean setRTS, boolean setDTR) throws Exception/* throws SerialException*/ {
-    //if (port != null) port.close();
-    //this.parent = parent;
-    //parent.attach(this);
-
-    resetDecoding(StandardCharsets.UTF_8);
-
-    int parity = SerialPort.PARITY_NONE;
-    if (iparity == 'E') parity = SerialPort.PARITY_EVEN;
-    if (iparity == 'O') parity = SerialPort.PARITY_ODD;
-
-    int stopbits = SerialPort.STOPBITS_1;
-    if (istopbits == 1.5f) stopbits = SerialPort.STOPBITS_1_5;
-    if (istopbits == 2) stopbits = SerialPort.STOPBITS_2;
-
-    // This is required for unit-testing
-    if (iname.equals("none")) {
-      return;
-    }
-
+  public Serial(String iname) throws Exception {
     try {
       port = new SerialPort(iname);
       port.openPort();
-      boolean res = port.setParams(irate, idatabits, stopbits, parity, setRTS, setDTR);
+      boolean res = port.setParams(9600, 8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE, true, true);
       if (!res) {
-        System.err.println("Error while setting serial port parameters");//: " + irate, iparity, idatabits, istopbits{0} {1} {2} {3}"),
+        System.err.println("Error while setting serial port parameters");
       }
       port.addEventListener(this);
     } catch (SerialPortException e) {
@@ -148,19 +61,15 @@ public class Serial implements SerialPortEventListener {
     }
 
     if (port == null) {
-      throw new Exception("Serial port " + iname + " not found. Did you select the right one from the Tools > Serial Port menu?");
+      throw new Exception("Serial port " + iname + " not found");
     }
-  }
-
-  public void setup() {
-    //parent.registerCall(this, DISPOSE);
   }
 
   public void dispose() throws IOException {
     if (port != null) {
       try {
         if (port.isOpened()) {
-          port.closePort();  // close the port
+          port.closePort();
         }
       } catch (SerialPortException e) {
         throw new IOException(e);
@@ -174,139 +83,22 @@ public class Serial implements SerialPortEventListener {
   public synchronized void serialEvent(SerialPortEvent serialEvent) {
     if (serialEvent.isRXCHAR()) {
       try {
-        byte[] buf = port.readBytes(serialEvent.getEventValue());
-        processSerialEvent(buf);
+        @SuppressWarnings("unused")
+		byte[] buf = port.readBytes(serialEvent.getEventValue());
+        
+        // do something with `buf` here
+        
       } catch (SerialPortException e) {
         errorMessage("serialEvent", e);
       }
     }
   }
 
-  public void processSerialEvent(byte[] buf) {
-    int next = 0;
-    // This uses a CharsetDecoder to convert from bytes to UTF-8 in
-    // a streaming fashion (i.e. where characters might be split
-    // over multiple reads). This needs the data to be in a
-    // ByteBuffer (inFromSerial, which we also use to store leftover
-    // incomplete characters for the nexst run) and produces a
-    // CharBuffer (outToMessage), which we then convert to char[] to
-    // pass onwards.
-    // Note that these buffers switch from input to output mode
-    // using flip/compact/clear
-    while (next < buf.length || inFromSerial.position() > 0) {
-      do {
-        // This might be 0 when all data was already read from buf
-        // (but then there will be data in inFromSerial left to
-        // decode).
-        int copyNow = Math.min(buf.length - next, inFromSerial.remaining());
-        inFromSerial.put(buf, next, copyNow);
-        next += copyNow;
-
-        inFromSerial.flip();
-        bytesToStrings.decode(inFromSerial, outToMessage, false);
-        inFromSerial.compact();
-
-        // When there are multi-byte characters, outToMessage might
-        // still have room, so add more bytes if we have any.
-      } while (next < buf.length && outToMessage.hasRemaining());
-
-      // If no output was produced, the input only contained
-      // incomplete characters, so we're done processing
-      if (outToMessage.position() == 0)
-        break;
-
-      outToMessage.flip();
-      char[] chars = new char[outToMessage.remaining()];
-      outToMessage.get(chars);
-      message(chars, chars.length);
-      outToMessage.clear();
-    }
-  }
-
-  /**
-   * This method is intented to be extended to receive messages
-   * coming from serial port.
-   */
-  protected void message(char[] chars, int length) {
-    // Empty
-  }
-
-
-  /**
-   * This will handle both ints, bytes and chars transparently.
-   */
-  public void write(int what) {  // will also cover char
-    try {
-      port.writeInt(what & 0xff);
-    } catch (SerialPortException e) {
-      errorMessage("write", e);
-    }
-  }
-
-
-  public void write(byte bytes[]) {
+  public void write(byte[] bytes) {
     try {
       port.writeBytes(bytes);
     } catch (SerialPortException e) {
       errorMessage("write", e);
     }
-  }
-
-
-  /**
-   * Write a String to the output. Note that this doesn't account
-   * for Unicode (two bytes per char), nor will it send UTF8
-   * characters.. It assumes that you mean to send a byte buffer
-   * (most often the case for networking and serial i/o) and
-   * will only use the bottom 8 bits of each char in the string.
-   * (Meaning that internally it uses String.getBytes)
-   * <p>
-   * If you want to move Unicode data, you can first convert the
-   * String to a byte stream in the representation of your choice
-   * (i.e. UTF8 or two-byte Unicode data), and send it as a byte array.
-   */
-  public void write(String what) {
-    write(what.getBytes());
-  }
-
-  public void setDTR(boolean state) {
-    try {
-      port.setDTR(state);
-    } catch (SerialPortException e) {
-      errorMessage("setDTR", e);
-    }
-  }
-
-  public void setRTS(boolean state) {
-    try {
-      port.setRTS(state);
-    } catch (SerialPortException e) {
-      errorMessage("setRTS", e);
-    }
-  }
-
-  /**
-   * Reset the encoding used to convert the bytes coming in
-   * before they are handed as Strings to {@Link #message(char[], int)}.
-   */
-  public synchronized void resetDecoding(Charset charset) {
-    bytesToStrings = charset.newDecoder()
-                      .onMalformedInput(CodingErrorAction.REPLACE)
-                      .onUnmappableCharacter(CodingErrorAction.REPLACE)
-                      .replaceWith("\u2e2e");
-  }
-
-  //static public List<String> list() {
-  //  return Arrays.asList(SerialPortList.getPortNames());
-  //}
-
-
-  /**
-   * General error reporting, all corraled here just in case
-   * I think of something slightly more intelligent to do.
-   */
-  private static void errorMessage(String where, Throwable e) {
-    System.err.println("Error inside Serial.{0}()");//), where));
-    e.printStackTrace();
   }
 }
